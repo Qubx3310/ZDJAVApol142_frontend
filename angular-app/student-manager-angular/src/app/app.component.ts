79a460d6dcc0ef734e6ea151b2b975c2254570ed
sobierajpawel <sobierajpawel@o2.pl>
2024-01-14 15:47:12 +0100

    Adding list students

--- angular-app/student-manager-angular/src/app/app.component.ts
+++ angular-app/student-manager-angular/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { StudentListComponent } from "./student-list/student-list.component";
+import { AddStudentComponent } from "./add-student/add-student.component";
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, StudentListComponent]
+    imports: [CommonModule, RouterOutlet, StudentListComponent, AddStudentComponent, NavigationComponent]
 })
 export class AppComponent {
   title = 'student-manager-angular';
--- angular-app/student-manager-angular/src/app/app.routes.ts
+++ angular-app/student-manager-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
+import { AddStudentComponent } from './add-student/add-student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"students", component: StudentListComponent},
+    {path:"add-student", component:AddStudentComponent}
+];
